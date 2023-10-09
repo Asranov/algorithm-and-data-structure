c6f5a66b303eba3fe75f8877e561658a2ac4ebd8
Asranov Baxtinur <asranov0003@gmail.com>
2023-10-09 10:50:54 +0500

    o(n) and 0(2n)

--- voxmind.js
+++ voxmind.js
@@ -1,1 +1,21 @@
-console.log("HELLO ALGORITHM AND DATA STRUCTURE");
+// Data Structures
+// 1.Linked Lists
+// 2.Doubly Linked Lists
+// 3.Stack & Queues
+// 4.Binar Search Trees
+// 5.Hash Tables
+// 6.Graphs
+
+// 0(n)
+function logItems(n) {
+  for (let i = 0; i < n; i++) {
+    console.log("i: ", i);
+  }
+
+  // 0(2n)
+  for (let j = 0; j < n; j++) {
+    console.log("j: ", j);
+  }
+}
+
+logItems(5);
