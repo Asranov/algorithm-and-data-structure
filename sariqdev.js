0d77e7c2185e272c03dd31c958114f10cab1bc58
Asranov Baxtinur <asranov0003@gmail.com>
2023-10-10 11:04:31 +0500

    intro data structure and algorithm

---
+++ sariqdev.js
@@ -0,0 +1,10 @@
+//DATA STRUCTER & ALGORITHM
+
+// Algorithms
+// Algoritm - muammoning qadam-baqadam yechimi
+// -Bitta muammo uchun bir qancha yechim bo'lishi mumkun (kak bu recept ovqat)
+
+// Malumotlar - komputer xotirasida saqlanuvchi matn, rasm, video, audio, video, dasturlar vahokazo
+
+// Data Structure
+// Malumotlar tuzilmasi - malumotlardan samarali foydalanish uchun tartibga solishning o'ziga xos usuli
