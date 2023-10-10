da6c4f690f3b0fb987270f855218a54132e89fc4
Asranov Baxtinur <asranov0003@gmail.com>
2023-10-10 11:20:56 +0500

    advantages of theme

--- sariqdev.js
+++ sariqdev.js
@@ -8,3 +8,7 @@
 
 // Data Structure
 // Malumotlar tuzilmasi - malumotlardan samarali foydalanish uchun tartibga solishning o'ziga xos usuli
+
+// Fanning dolzarbligi
+// Mavjud muammoni bir necha yechimi bolishi mumkun va eng samaraligini ishlatish
+// Agar bu fanni bilsa muammoni yechimini samarali yolini topish tez boladi
