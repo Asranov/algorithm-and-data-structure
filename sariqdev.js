e0b195be39ae1499dc6b1a483f6378bd943a8c44
Asranov Baxtinur <asranov0003@gmail.com>
2023-10-11 10:49:34 +0500

    What is Algorithm

--- sariqdev.js
+++ sariqdev.js
@@ -15,4 +15,26 @@
 // Mavjud muammoni bir necha yechimi bolishi mumkun va eng samaraligini ishlatish
 // Agar bu fanni bilsa muammoni yechimini samarali yolini topish tez boladi
 
-// #1
+// #01 ALGORITMLAR | ALGORITM NIMA?
+// Algoritm - bu muammoni hal qilish uchun aniq ketma-ketlikda beriladigon ko'rsatmalar to'plami
+// Yaxshi algoritmning belgilari: Muammo va yechim aniq berilgan bo'lishi kerak, Algoritmning bosqichi aniq va rovchan bolishi kerak
+
+function getSquare(a) {
+  return a * a;
+}
+
+// Mashqlar
+// 1.Foydalanuvchi kiritgan ikki sonni qo'shish algoritmi:
+// -1. Start
+// -2. son1, son2, summa o'zgaruvchilarini yaratamiz
+// -3. son1, son2 qiymatlarini qabul qilamiz
+// -4. son1, son2 larni qo'shamiz va summa ga yuklaymiz: summa = son1 + son2
+// -5. summani qaytaramiz
+// -6. Stop
+
+function getSum(a, b) {
+  const sum = 0;
+  sum = a + b;
+
+  return sum;
+}
