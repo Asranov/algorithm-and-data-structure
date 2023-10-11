2fa185c0567b852fd637f4a8e7361a5e5707b18b
Asranov Baxtinur <asranov0003@gmail.com>
2023-10-11 10:22:57 +0500

    translate text

--- sariqdev.js
+++ sariqdev.js
@@ -2,13 +2,14 @@
 // DATA STRUCTURE & ALGORITHM
 
 // Algorithms
-// Algoritm - muammoning qadam-baqadam yechimi
+// Algoritm - muammoning qadam-baqadam yechimi (пошаговое решение проблемы)
 // -Bitta muammo uchun bir qancha yechim bo'lishi mumkun (kak bu recept ovqat)
 
 // Malumotlar - komputer xotirasida saqlanuvchi matn, rasm, video, audio, video, dasturlar vahokazo
 
 // Data Structure
 // Malumotlar tuzilmasi - malumotlardan samarali foydalanish uchun tartibga solishning o'ziga xos usuli
+// (программная единица, позволяющая хранить и обрабатывать однотипные и/или логически связанные данные. )
 
 // Fanning dolzarbligi
 // Mavjud muammoni bir necha yechimi bolishi mumkun va eng samaraligini ishlatish
