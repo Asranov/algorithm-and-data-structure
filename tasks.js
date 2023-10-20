74b5369cbb0c7b652496fe33aed11776280cdc1d
Asranov Baxtinur <asranov0003@gmail.com>
2023-10-20 10:29:08 +0500

    search index

--- tasks.js
+++ tasks.js
@@ -1,14 +1,18 @@
-// Write a program to check if a string or word or number is palindrome ?
-const isPlaindrome = (inputChar) => {
-  let str = inputChar.toString();
-  let resultWord = "";
-  for (let i = str.length - 1; i >= 0; i--) {
-    resultWord += str[i];
-  }
-  return resultWord == str;
-};
+// Linear Search Tasks:
+// Task 1:
+// Write a JavaScript function that performs a linear search on an array of numbers. The function should take two arguments:
+// An array of numbers.
+// A target number to search for.
+// The function should return the index of the target number in the array if it exists, or -1 if the target number is not found in the array.
 
-console.log(isPlaindrome("racecar"));
-console.log(isPlaindrome("abc"));
-console.log(isPlaindrome(121));
-console.log(isPlaindrome(343));
+function searchIndex(arr, target) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === target) {
+      return i;
+    }
+  }
+
+  return -1;
+}
+
+searchIndex([3, 2, 4, 1, 5, 2], 2);
